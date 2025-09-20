1ab7ebb50e958d56284424736ec4e5c3eaedc1d6
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2025-09-20 19:15:04 +0500

    fixed vercel.json again

---
+++ node-server/src/api/server.js
@@ -0,0 +1,14 @@
+const app = require("../src/app.js");
+const config = require("../src/config/index.js");
+
+const PORT = config.port;
+
+app.get("/", (req, res) => {
+  res.status(200).send("How are you brothers");
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
