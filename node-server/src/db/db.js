73d24d89fbba22a5e98bfa405a79d6c811ba1858
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2025-09-20 22:47:02 +0500

    Commented sequelize code

--- node-server/src/db/db.js
+++ node-server/src/db/db.js
@@ -1,23 +1,23 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
 
-// Option 3: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize(process.env.DB_URL);
+// // Option 3: Passing parameters separately (other dialects)
+// // const sequelize = new Sequelize(process.env.DB_URL);
 
-const sequelize = new Sequelize("postgres", "postgres", "safee", {
-  host: "localhost",
-  dialect: "postgres",
-});
+// const sequelize = new Sequelize("postgres", "postgres", "safee", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
 
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
 
-// connectDB();
+// // connectDB();
 
-module.exports = { sequelize, connectDB };
+// module.exports = { sequelize, connectDB };
--- node-server/src/models/usermodels.js
+++ node-server/src/models/usermodels.js
@@ -1,22 +1,22 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const { sequelize } = require("../db/db.js");
+// const { Sequelize, Model, DataTypes } = require("sequelize");
+// const { sequelize } = require("../db/db.js");
 
-const HuggingFaceModels = sequelize.define(
-  "postgres",
-  {
-    Inference_Provider: DataTypes.TEXT,
-    Model_Name: DataTypes.TEXT,
-    Supported_Feature: DataTypes.TEXT,
-  },
-  {
-    tableName: "Hugging Face Models",
-    timestamps: false,
-  }
-);
+// const HuggingFaceModels = sequelize.define(
+//   "postgres",
+//   {
+//     Inference_Provider: DataTypes.TEXT,
+//     Model_Name: DataTypes.TEXT,
+//     Supported_Feature: DataTypes.TEXT,
+//   },
+//   {
+//     tableName: "Hugging Face Models",
+//     timestamps: false,
+//   }
+// );
 
-// (async () => {
-//   await sequelize.sync({ alter: true });
-//   // Code here
-// })();
+// // (async () => {
+// //   await sequelize.sync({ alter: true });
+// //   // Code here
+// // })();
 
-module.exports = { HuggingFaceModels };
+// module.exports = { HuggingFaceModels };
